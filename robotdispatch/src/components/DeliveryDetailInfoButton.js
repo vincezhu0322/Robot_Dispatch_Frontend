31cc1049c66add9f74f368c522b5a626cb593f2a
vincezhu0322 <115663648+vincezhu0322@users.noreply.github.com>
2022-11-04 11:57:52 -0700

    Update DeliveryDetailInfoButton.js

--- robotdispatch/src/components/DeliveryDetailInfoButton.js
+++ robotdispatch/src/components/DeliveryDetailInfoButton.js
@@ -50,20 +50,39 @@ class DeliveryDetailInfoButton extends React.Component {
             footer={null}
             onCancel={this.handleCancel}
           >
-              <Text strong={true}>Delivery Id: </Text>
-              <Text type="secondary">{delivery.id}</Text>
+              <Text strong={true}>Delivery ID: </Text>
+              <Text type="secondary">{delivery.order_id}</Text>
               <Divider/>
-              <Text strong={true}>Delivery Status: </Text>
-              <Text type="secondary">{delivery.status}</Text>
+              <Text strong={true}>Sent By: </Text>
+              <Text type="secondary">{delivery.pickup_address}</Text> 
               <Divider/>
-              <Text strong={true}>Delivery Location: </Text>
-              <Text type="secondary">{delivery.location.address}</Text> 
+              <Text strong={true}>Sent To: </Text>
+              <Text type="secondary">{delivery.deliver_address}</Text>
               <Divider/>
-              <Text strong={true}>Start Date: </Text>
-              <Text type="secondary">{delivery.start_date}</Text>
+              <Text strong={true}>Order Create By: </Text>
+              <Text type="secondary">{delivery.guest.username}</Text>
               <Divider/>
-              <Text strong={true}>End Date: </Text>
-              <Text type="secondary">{delivery.end_date}</Text>
+              <Text strong={true}>Order Create Date: </Text>
+              <Text type="secondary">{delivery.order_date}</Text>
+              <Divider/>
+              <Text strong={true}>Pickup Time (Expected): </Text>
+              <Text type="secondary">{delivery.expect_pickup_time}</Text>
+              <Divider/>
+              <Text strong={true}>Delivery Date (Expected): </Text>
+              <Text type="secondary">{delivery.expect_delivery_date}</Text>
+              <Divider/>
+              <Text strong={true}>Arrive Date: </Text>
+              <Text type="secondary">{delivery.delivered_date}</Text>
+              <Divider/>
+              <Text strong={true}>Item Detail: </Text>
+              <Text type="secondary">
+                Type: {delivery.description} /
+                Size: {delivery.length} x {delivery.width} x {delivery.height} /
+                Weight: {delivery.weight}
+              </Text>
+              <Divider/>
+              <Text strong={true}>Center ID: </Text>
+              <Text type="secondary">{delivery.centerId.id}</Text>
           </Modal>
         )}
       </React.Fragment>
